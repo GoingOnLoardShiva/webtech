9289818be2122a52a7efda7cb3c6c61cb7663697
Your Name <your.email@example.com>
2026-01-05 21:39:31 +0530

    update ads code

--- Nextjs-tailwindcss-blog-template/src/app/layout.js
+++ Nextjs-tailwindcss-blog-template/src/app/layout.js
@@ -62,6 +62,7 @@ export default function RootLayout({ children }) {
           name="google-site-verification"
           content="bIAcXNOt6yIh3QD-kye1XlO0LAt81zxtUjZH2VKdFNY"
         />
+        <script src="https://confessinvaluable.com/17/ca/c4/17cac43918f11f42daa0cff0fdb02ff7.js"></script>
       </head>
 
       <body
@@ -101,6 +102,7 @@ export default function RootLayout({ children }) {
           `}
         </Script>
         <script src="https://pl28407662.effectivegatecpm.com/17/ca/c4/17cac43918f11f42daa0cff0fdb02ff7.js"></script>
+        
         <script async="async" data-cfasync="false" src="https://pl28407725.effectivegatecpm.com/1ac2e82cee1612d6a34d01b4b05400c2/invoke.js"></script>
         <div id="container-1ac2e82cee1612d6a34d01b4b05400c2"></div>
         
