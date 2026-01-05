ca1d12a709209a17a190c9d1bf446e90379792ed
Your Name <your.email@example.com>
2026-01-05 11:52:50 +0530

    update or sum

--- Nextjs-tailwindcss-blog-template/src/app/api/sharedapi/route.js
+++ Nextjs-tailwindcss-blog-template/src/app/api/sharedapi/route.js
@@ -1,29 +1,34 @@
-import { NextResponse } from 'next/server';
-import ConnectDb from '@/src/lib/mongoose';
-import Post from '@/src/models/Post';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
+import { NextResponse } from "next/server";
+import ConnectDb from "@/src/lib/mongoose";
+import Post from "@/src/models/Post";
 
-///create  a shared api for sharing my posts with cecking blog_user_secret_key match my secret key//
 export async function GET(req) {
-    try{
-        const body = await req.json();
-        const { apiKey } = body;
-        if (!apiKey) return NextResponse.json({ error: 'Missing secret key' }, { status: 400 });
-        const secretKey = "ren_555_snjj_A08"
-        //compare the secret key with env secret key//
-        if(apiKey !== secretKey){
-            return NextResponse.json({ error: 'Invalid secret key' }, { status: 401 });
-        }
-        //find the letest blog post //
-        await ConnectDb();
-        const post = await Post.findOne().sort({ createdAt: -1 }).select('-password');
-        if(!post){
-            return NextResponse.json({ error: 'No post found' }, { status: 404 });
-        }
-        return NextResponse.json({ post }, { status: 200 });
-    }catch(err){
-        console.error(err);
-        return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  try {
+    const apiKey = req.headers.get("x-api-key");
+    const BLOG_SHARED_API_KEY ="ren_555_snjj_A08"
+
+
+    if (!apiKey) {
+      return NextResponse.json({ error: "Missing API key" }, { status: 400 });
     }
+
+    if (apiKey !== BLOG_SHARED_API_KEY) {
+      return NextResponse.json({ error: "Invalid API key" }, { status: 401 });
+    }
+
+    await ConnectDb();
+
+    const post = await Post.findOne()
+      .sort({ createdAt: -1 })
+      .select("title slug content createdAt author image");
+
+    if (!post) {
+      return NextResponse.json({ error: "No post found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ post });
+  } catch (err) {
+    console.error("Shared API error:", err);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
 }
